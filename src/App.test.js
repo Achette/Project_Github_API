174101f3b9b65a2d6dc5c546d42fa8b8cdae79c8
Achette <i.achette@live.com>
2022-08-28 12:04:43 -0300

    Project created and some adjusts

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders devsamurai message', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Fala Samurai/i);
   expect(linkElement).toBeInTheDocument();
 });
